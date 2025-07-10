1046eeed6c38a6689837224f85ad1581f4de2820
antony-akshay <akshay.dev.ac.in@gmail.com>
2025-07-10 22:58:11 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-green-800">
+        sqaris
+      </h1>
+    </>
+  )
+}
+
+export default App
